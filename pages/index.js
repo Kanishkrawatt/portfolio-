23df28ae1fcf68e4851cfeea75f6dd699e948546
Kanishk <rawat27kanishk@gmail.com>
2022-08-29 16:54:46 +0530

    Get Static Props

--- pages/index.js
+++ pages/index.js
@@ -35,6 +35,6 @@ export async function getStaticProps() {
   const Data = Alldata[0].data;
   return{
     props: { Data }, // will be passed to the page component as props
-    unstable_revalidate:60,
+    unstable_revalidate: 1
   };
 }
