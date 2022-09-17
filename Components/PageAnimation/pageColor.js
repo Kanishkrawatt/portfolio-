4d78923d7167989abcabc9f980480aa6b4dd2fd4
Kanishk <rawat27kanishk@gmail.com>
2022-09-17 20:22:03 +0530

    Fix: Background Color Fix

--- Components/PageAnimation/pageColor.js
+++ Components/PageAnimation/pageColor.js
@@ -4,25 +4,34 @@
 import React,{useState,useEffect} from 'react'
 import useScrollPosition from "../Hooks/useScreen";
 import GetPosition from '../Hooks/getPosition';
-const PageColorAnimation= (ScrollElements) => {
+const PageColorAnimation= () => {
     const position = useScrollPosition();
-    
     useEffect(() => {
-        let Body = document.querySelector("body")
+      let Body = document.querySelector("body")
       let Home = GetPosition("Home");
-      let AboutPos= GetPosition("About")+400;
-      let ProjectPos = GetPosition("Project")+400;
-      let ContactPos = GetPosition("Contact")+400;
-      let TimeLinePos = GetPosition("Path")+400;
-        if (position > Home && position < AboutPos) {
+      let AboutPos= GetPosition("About");
+      let ProjectPos = GetPosition("Project");
+      let ContactPos = GetPosition("Contact");
+      let TimeLinePos = GetPosition("Path");
+      // Home 0 About 916 Project 1832 Contact 3904 Timeline 2748
+      console.log("Home",Home,"About",AboutPos,"Project",ProjectPos,"Contact",ContactPos,"Timeline",TimeLinePos);
+        if (position > Home && position < AboutPos-(AboutPos/2)) {
+          // Body.style.backgroundColor = "#c1d5a4";
+          Body.style.backgroundColor = "#9ED2C6";
+
+        } else if (position > AboutPos-(AboutPos/2) && position < ProjectPos-(AboutPos/2)) {
           Body.style.backgroundColor = "#c1d5a4";
-        } else if (position > AboutPos && position < ProjectPos) {
+
+          // Body.style.backgroundColor = "#ffd07f";
+        } else if (position > ProjectPos-(AboutPos/2) && position < TimeLinePos-(AboutPos/2)) {
+          // Body.style.backgroundColor = "#afb4ff";
           Body.style.backgroundColor = "#ffd07f";
-        } else if (position > ProjectPos && position < TimeLinePos) {
+
+        } else if (position > TimeLinePos-(AboutPos/2) && position < ContactPos-(AboutPos/2)) {
           Body.style.backgroundColor = "#afb4ff";
-        } else if (position > TimeLinePos && position < ContactPos) {
-          Body.style.backgroundColor = "#FFE69A";} 
-        else if (position > ContactPos) {
+          
+        } 
+        else if (position > ContactPos-(AboutPos/2)) {
           Body.style.backgroundColor = "#FFE69A";
         }
          else {
