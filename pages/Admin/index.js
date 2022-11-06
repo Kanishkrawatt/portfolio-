bf255da3bc7f67b161b1793b0dc42a57fd5a3283
Kanishk <rawat27kanishk@gmail.com>
2022-11-06 20:55:39 +0530

    Added Admin page and api

--- pages/Admin/index.js
+++ pages/Admin/index.js
@@ -1,10 +1,21 @@
 import React from 'react'
-import AdminDashBoard from "../../Components/AdminDashBoard"
+import AdminDashBoard from "../../Components/AdminDashBoard/Admin"
+import db from '../../db'
 
-const index = () => {
+const index = (props) => {
+  let Data = props.Data;
   return (
-    <AdminDashBoard />
+    <AdminDashBoard Data={Data} />
   )
 }
 
 export default index
+
+export async function getStaticProps() {
+  let data = await db.collection("contact").get();
+  let Data = data.docs.map((entry) => entry.data());
+  return {
+    props: { Data }, // will be passed to the page component as props
+    revalidate: 1,
+  };
+}
