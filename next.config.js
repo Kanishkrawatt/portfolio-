258c179202da8c7b1b5bad70c635c13faeb72548
Kanishk Rawat <rawat27kanishk@gmail.com>
2023-03-23 04:39:00 +0530

    Updated next config

--- next.config.js
+++ next.config.js
@@ -2,6 +2,24 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  async rewrites() {
+    return [
+      {
+        source: '/blog',
+        destination: 'https://www.blog.ikanishk.me/',
+      },
+      {
+        source:"/instagram",
+        destination:'https://www.instagram.com/kanishk_rawatt/',
+      },
+      {
+        source:"/github",
+        destination:'https://github.com/kanishkrawatt',
+      }
+
+    ]
+  },
+
 };
 
 module.exports = nextConfig;
