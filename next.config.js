41752d765722a698da6eb6caf1587b42cc7f0344
Kanishk Rawat <rawat27kanishk@gmail.com>
2023-03-23 18:55:55 +0530

    Added Redirects

--- next.config.js
+++ next.config.js
@@ -2,24 +2,36 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-  async rewrites() {
+  async redirects() {
     return [
       {
-        source: '/blog',
-        destination: 'https://www.blog.ikanishk.me/',
+        source: "/blog",
+        destination: "https://overlow.ikanishk.me/",
+        permanent: true,
       },
       {
-        source:"/instagram",
-        destination:'https://www.instagram.com/kanishk_rawatt/',
+        source: "/overlow",
+        destination: "https://overlow.ikanishk.me/",
+        permanent: true,
       },
       {
-        source:"/github",
-        destination:'https://github.com/kanishkrawatt',
-      }
-
-    ]
+        source: "/github",
+        destination: "https://github.com/kanishkrawatt",
+        permanent: true,
+      },
+      {
+        source: "/linkedin",
+        destination: "https://www.linkedin.com/in/kanishk-rawat-1362ab231/",
+        permanent: true,
+      },
+      {
+        source: "/mail",
+        destination:
+          "https://mail.google.com/mail/u/0/#inbox?compose=CllgCJZWQDCcVFGBBhvHqrftmPkGfQvvsFZMHXNrzksmqsRbCcwLWXFJbsFgjlbZGJtKWLQdzfg",
+        permanent: true,
+      },
+    ];
   },
-
 };
 
 module.exports = nextConfig;
