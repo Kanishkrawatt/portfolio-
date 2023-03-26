3a58b4fca7d066e77c950de5bab0bf6f57ad97f5
Kanishk Rawat <rawat27kanishk@gmail.com>
2023-03-26 19:42:37 +0530

    Added Firebase Funciton

--- db/firebasefunction.js
+++ db/firebasefunction.js
@@ -1,76 +1,55 @@
-import React from "react";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  onAuthStateChanged,
+  signOut,
+} from "firebase/auth";
+import { auth } from "../db";
 
-import firebase from "firebase/compat/app";
-import "firebase/compat/auth";
-import "firebase/compat/firestore";
-
-import { useAuthState } from "react-firebase-hooks/auth";
-
-import { firebaseconfig } from "./firebaseconfig";
-firebase.initializeApp(firebaseconfig);
-
-const auth = firebase.auth();
-// const firestore = firebase.firestore();
-const user = auth.user;
-const signInWithGoogle = () => {
-  const provider = new firebase.auth.GoogleAuthProvider();
-  auth.signInWithPopup(provider);
+export const Login = (email, password) => {
+  createUserWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      console.log("success");
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log("ErroCode: ", errorCode, "ErrorMessage: ", errorMessage);
+    });
 };
-const signOutWithGoogle = () => {
-  auth.signOut();
-};
-function SignIn() {
-  const [user] = useAuthState(auth);
-  let userData = {};
-  if (user !== null) {
-    userData = {
-      uid: user.uid,
-      displayName: user.displayName,
-      email: user.email,
-      photoURL: user.photoURL,
-      emailVerified: user.emailVerified,
-    };
-  }
-  return <>{!user && signInWithGoogle()}</>;
-}
 
-export const CheckUser = () => {
-  const [user] = useAuthState(auth);
-  if (user !== null) {
-    return 1;
-  }
-  return 0;
+export const SignIn = (email, password) => {
+  signInWithEmailAndPassword(auth, email, password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      return user;
+    })
+    .catch((error) => {
+      const errorCode = error.code;
+      const errorMessage = error.message;
+      console.log("ErroCode: ", errorCode, "ErrorMessage: ", errorMessage);
+    });
 };
-export const UserInfo = () => {
-  const [user] = useAuthState(auth);
-  let userData = {};
-  if (user !== null) {
-    userData = {
-      uid: user.uid,
-      displayName: user.displayName,
-      email: user.email,
-      photoURL: user.photoURL,
-      emailVerified: user.emailVerified,
-    };
-    return userData;
-  }
-  return null;
-};
-export const IsAdmin = () => {
-  const [user] = useAuthState(auth);
-  if (user !== null) {
-    const admin = [{ username: "Kanishk rawat" }];
-    // admin.forEach((a) => {
-    //   if (user.displayName == a.username) {
-    //     return 1;
-    //   }
-    // });
 
-    if (user.displayName === admin[0].username) {
-      return 1;
+// Signout
+
+export const SignOut = () => {
+  signOut(auth)
+    .then(() => {
+      // Sign-out successful.
+    })
+    .catch((error) => {
+      // An error happened.
+    });
+};
+
+export const onAuthStateChange = (setUser) => {
+  return auth.onAuthStateChanged((user) => {
+    if (user) {
+      setUser({ loggedIn: true, email: user.email });
+    } else {
+      setUser({ loggedIn: false, email: "" });
     }
-  }
-  return 0;
+  });
 };
-export default SignIn;
-export { signInWithGoogle, signOutWithGoogle, user };
