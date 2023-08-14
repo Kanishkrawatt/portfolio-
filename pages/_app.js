51649f3f58ce9d2bbb9bdb49213eaa6194dcb488
Kanishk Rawat <rawat27kanishk@gmail.com>
2023-08-14 15:29:07 +0530

    Added Google analytics

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,12 @@
 import "../styles/globals.css";
 import React, { useState, useEffect } from "react";
 import Head from "next/head";
+import Script from "next/script";
 import OnLoadAnimation from "../Components/OnLoadAnimation/loadingAnimation";
 
 function MyApp({ Component, pageProps }) {
   const [loading, setLoading] = useState(true);
 
-  // set loading until the page is loaded no setTimeout
   useEffect(() => {
     setTimeout(() => {
       if (document.readyState === "complete") {
@@ -21,6 +21,18 @@ function MyApp({ Component, pageProps }) {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <title>| Kanishk</title>
       </Head>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-xxxxxxxxxx"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', '${process.env.NEXT_PUBLIC_FB_measurementId}');
+        `}
+      </Script>
       {!loading ? <Component {...pageProps} /> : <OnLoadAnimation />}
     </>
   );
